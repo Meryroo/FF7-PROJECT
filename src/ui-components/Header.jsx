d415169587667e6d5123258493a015272b6dc4cd
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-02-24 00:15:06 +0100

    headings header y navbar

---
+++ src/ui-components/Header.jsx
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+const HeaderStyled = styled.div`
+  display: flex;
+  justify-content: ${({ justify }) =>
+    justify === 'center'
+      ? 'center'
+      : justify === 'flex-end'
+      ? 'flex-end'
+      : justify === 'flex-star'
+      ? 'flex-star'
+      : justify === 'space-around'
+      ? 'space-around'
+      : justify === 'space-between'
+      ? 'space-between'
+      : 'center'};
+  align-items: ${({ align }) =>
+    align === 'center'
+      ? 'center'
+      : align === 'flex-end'
+      ? 'flex-end'
+      : align === 'flex-star'
+      ? 'flex-star'
+      : align === 'space-around'
+      ? 'space-around'
+      : align === 'space-between'
+      ? 'space-between'
+      : 'center'};
+  background-color: ${({ color }) => (color ? color : '#10101C')};
+  color: ${({ color }) => (color ? color : 'inherit')};
+  height: ${({ height }) => height};
+  width: 100%;
+  padding: ${({ padding }) => padding};
+`;
+const Header = ({ children, justify, align, color, height, padding }) => {
+  return (
+    <HeaderStyled
+      justify={justify}
+      align={align}
+      color={color}
+      height={height}
+      padding={padding}
+    >
+      {children}
+    </HeaderStyled>
+  );
+};
+
+export default Header;
