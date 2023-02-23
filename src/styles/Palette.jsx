da6417ac70a62065506cd6fe1c343dce66eb0cc4
meryroo <meryrooprada@gmail.com>
2023-02-23 19:16:51 +0100

    user context

--- src/styles/Palette.jsx
+++ src/styles/Palette.jsx
@@ -1,9 +1,11 @@
 const Palette = {
-    main: {background: '#10101C',
+  main: {
+    background: '#10101C',
     primary: '#E8DCB6',
     secondary: '#526F80',
     highlight: '#517388',
     light: '#83847E',
-    dark: '#83847E'}
-}
-export default Palette
+    dark: '#83847E',
+  },
+};
+export default Palette;
