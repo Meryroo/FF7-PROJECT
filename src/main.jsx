c60376be8bf1edc3b61ecffba62b20b4ad69e323
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-02-23 19:25:29 +0100

    user context y button

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import App from './App';
 import { PageContexProvider } from './context/PageContext';
+import { UserContexProvider } from './context/UserContext';
 import About from './pages/About';
 import Bestiary from './pages/Bestiary';
 import Data from './pages/Data';
@@ -12,18 +13,20 @@ import Login from './pages/Login';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PageContexProvider>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route index element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/bestiary" element={<Bestiary />} />
-            <Route path="/data" element={<Data />} />
-            <Route path="/about" element={<About />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </PageContexProvider>
+    <UserContexProvider>
+      <PageContexProvider>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route index element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/bestiary" element={<Bestiary />} />
+              <Route path="/data" element={<Data />} />
+              <Route path="/about" element={<About />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </PageContexProvider>
+    </UserContexProvider>
   </React.StrictMode>,
 );
--- src/ui-components/Button.jsx
+++ src/ui-components/Button.jsx
@@ -1,24 +1,19 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const ButtonStyled = styled.button`
   background-color: ${({ variant }) =>
-    variant === "dark" ? "#517388" : variant === "light" ? "#E8DCB6" : "white"};
-  border: ${({ border }) => (border === "yes" ? "2px solid black" : "none")};
-  color: ${({ color }) => (color === "white" ? "white" : "black")};
+    variant === 'dark' ? '#517388' : variant === 'light' ? '#E8DCB6' : 'white'};
+  border: ${({ border }) => (border === 'yes' ? '2px solid black' : 'none')};
+  color: ${({ color }) => (color === 'white' ? 'white' : 'black')};
   width: fit-content;
-  font-family: "Helvetica";
-  font-size: ${({ size }) =>
-    size === "lg" ? "25px" : size === "sm" ? "14px" : "20px"};
+  font-family: 'Helvetica';
+  font-size: ${({ size }) => (size === 'lg' ? '25px' : size === 'sm' ? '14px' : '20px')};
 
   font-weight: 700;
   letter-spacing: 0.5px;
   outline: none;
   padding: ${({ size }) =>
-    size === "lg"
-      ? "1.3rem 4.5rem"
-      : size === "sm"
-      ? "8px 16px"
-      : "0.8rem 4.3rem"};
+    size === 'lg' ? '1.3rem 4.5rem' : size === 'sm' ? '8px 16px' : '0.8rem 4.3rem'};
   text-transform: uppercase;
   transition: 0.5s ease-in-out;
   &:hover {
