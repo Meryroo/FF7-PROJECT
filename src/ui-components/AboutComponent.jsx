fa3534878d8fe6a9ace5d83e83a0243fa518edf8
Carlos Olano <carlos.olano@live.com>
2023-02-25 18:38:02 +0100

    feauture-about-update

--- src/ui-components/AboutComponent.jsx
+++ src/ui-components/AboutComponent.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 import Anchor from './Anchor';
+import DivFlex from './Divflex';
 import Image from './Image';
 
 const StyledDiv = styled.div`
@@ -18,27 +19,28 @@ const StyledDiv = styled.div`
   border: none;
 `;
 
-const AboutComponents = ({ img, imgname, link }) => {
+const AboutComponents = ({ img, imgname, link1, link2 }) => {
   return (
     <StyledDiv>
       <Image src={img} alt={imgname} width="100%" />
-
-      <Anchor href={link}>
-        <Image
-          src="https://res.cloudinary.com/dysog0ybg/image/upload/v1676300979/SocialMedia%20Icons/linkedin_rxabeq.png"
-          alt="linkedin"
-          width="25px"
-          height="25px"
-        />
-      </Anchor>
-      <Anchor href={link}>
-        <Image
-          src="https://res.cloudinary.com/dysog0ybg/image/upload/v1676300979/SocialMedia%20Icons/github_a3wdej.png"
-          alt="github"
-          width="25px"
-          height="25px"
-        />
-      </Anchor>
+      <DivFlex gap="1rem">
+        <Anchor link={link1}>
+          <Image
+            src="https://res.cloudinary.com/dysog0ybg/image/upload/v1676300979/SocialMedia%20Icons/linkedin_rxabeq.png"
+            alt="linkedin"
+            width="25px"
+            height="25px"
+          />
+        </Anchor>
+        <Anchor link={link2}>
+          <Image
+            src="https://res.cloudinary.com/dysog0ybg/image/upload/v1676300979/SocialMedia%20Icons/github_a3wdej.png"
+            alt="github"
+            width="25px"
+            height="25px"
+          />
+        </Anchor>
+      </DivFlex>
     </StyledDiv>
   );
 };
