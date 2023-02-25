505f0357c73b226b122dec512a7a8598e8eeda63
meryroo <meryrooprada@gmail.com>
2023-02-25 21:24:26 +0100

    feat home and footer

--- src/ui-components/Footer.jsx
+++ src/ui-components/Footer.jsx
@@ -1,7 +1,5 @@
 import styled from 'styled-components';
 
-import Palette from '../styles/Palette';
-
 const FooterStyled = styled.footer`
   display: flex;
   justify-content: ${({ justify }) =>
@@ -28,23 +26,28 @@ const FooterStyled = styled.footer`
       : align === 'space-between'
       ? 'space-between'
       : 'center'};
-  background-color: ${({ variant }) => (variant ? variant : Palette.main.background)};
-  color: ${({ color }) => (color ? color : 'inherit')};
+  background-color: ${({ variant }) => (variant ? variant : '#2d3e48')};
+  color: ${({ color }) => (color ? color : '#10101C')};
   height: ${({ height }) => height};
   width: 100%;
   padding: ${({ padding }) => padding};
   margin: ${(margin) => margin};
+  & .iconsfooter ul {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+  }
+  & .iconsfooter ul img {
+    height: 100%;
+    width: 26px;
+    filter: invert(90%);
+  }
+  & .footerscuared img {
+    filter: invert(90%);
+  }
 `;
-const Footer = ({
-  children,
-  justify,
-  align,
-  color,
-  height,
-  padding,
-  margin,
-  variant,
-}) => {
+const Footer = ({ justify, align, color, height, padding, margin, variant }) => {
   return (
     <FooterStyled
       justify={justify}
@@ -55,7 +58,40 @@ const Footer = ({
       margin={margin}
       variant={variant}
     >
-      {children}
+      <div className="footerscuared">
+        <img
+          src="https://res.cloudinary.com/dysog0ybg/image/upload/v1677165711/se_logo-3x-GCifjuXox_wlxvuy.webp"
+          alt="scuared Logo"
+        />
+      </div>
+      <div className="iconsfooter">
+        <ul>
+          <li>
+            <img
+              src="https://res.cloudinary.com/dnkacmdmh/image/upload/v1677165419/insta_tecmmc.webp"
+              alt="Istagram Icon"
+            />
+          </li>
+          <li>
+            <img
+              src="https://res.cloudinary.com/dnkacmdmh/image/upload/v1677165423/twi_kx2e6n.webp"
+              alt="Twitter Icon"
+            />
+          </li>
+          <li>
+            <img
+              src="https://res.cloudinary.com/dnkacmdmh/image/upload/v1677165421/face_spq3ck.webp"
+              alt="Facebook Icon"
+            />
+          </li>
+          <li>
+            <img
+              src="https://res.cloudinary.com/dnkacmdmh/image/upload/v1677165425/you_o61n62.webp"
+              alt="Youtube Icon"
+            />
+          </li>
+        </ul>
+      </div>
     </FooterStyled>
   );
 };
