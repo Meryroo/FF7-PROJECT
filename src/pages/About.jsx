f82ad548828ecdb649313e02ee30dc7ce475b3d0
Adrián <123007421+Kirk810@users.noreply.github.com>
2023-02-26 17:27:49 +0100

    feature-about-mack

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,31 +5,41 @@ import AboutComponents from '../ui-components/AboutComponent';
 import DivFlex from '../ui-components/Divflex';
 
 const About = () => {
-  const { page, setPage } = useContext(PageContext);
+  const { setPage } = useContext(PageContext);
   useEffect(() => {
     setPage('about');
   }, []);
   return (
     <DivFlex direction="column" padding="1rem" gap="30px">
-      <h1>{page}</h1>
+      <h1>EQUIPO</h1>
       <DivFlex gap="3rem">
         <AboutComponents
+          link1=""
+          link2="https://github.com/Meryroo"
           img="https://res.cloudinary.com/dysog0ybg/image/upload/v1677166274/9JM2umQBwkZPwTXA3vvWdf_myjjft.jpg"
           alt="mery"
+          name="Maria"
         />
         <AboutComponents
+          link1="https://www.linkedin.com/in/rafael-garc%C3%ADa-rivas-9b5303264/"
+          link2="https://github.com/Rgrivas9"
           img="https://res.cloudinary.com/dysog0ybg/image/upload/v1677166274/9JM2umQBwkZPwTXA3vvWdf_myjjft.jpg"
           alt="rafa"
+          name="Rafa"
         />
         <AboutComponents
+          link1="https://www.linkedin.com/in/adrian-soto-9b66a9246/"
+          link2="https://github.com/Kirk810"
           img="https://res.cloudinary.com/dysog0ybg/image/upload/v1677166274/9JM2umQBwkZPwTXA3vvWdf_myjjft.jpg"
           alt="adri"
+          name="Adrián"
         />
         <AboutComponents
           link1="https://www.linkedin.com/in/carlos-olano-gonz%C3%A1lez-338b3b68/"
           link2="https://github.com/olanovich3?tab=repositories"
           img="https://res.cloudinary.com/dysog0ybg/image/upload/v1677166274/9JM2umQBwkZPwTXA3vvWdf_myjjft.jpg"
           alt="carlos"
+          name="Carlos"
         />
       </DivFlex>
     </DivFlex>
