e0c0bffa8a2d15f9b342edc809a1e1cf821a51ee
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-02-26 12:57:53 +0100

    --

--- src/utils/GetMinMax.jsx
+++ src/utils/GetMinMax.jsx
@@ -24,11 +24,11 @@ const GetMinMax = (list, filter) => {
   if (filter.length == 1) {
     const lista = list.map((enemy) => enemy[filter[0]]).sort((a, b) => a - b);
     minMax.push(lista[0]);
-    minMax.push(lista[lista.length] - 1);
+    minMax.push(lista[lista.length - 1]);
   } else {
     const lista = list.map((enemy) => enemy[filter[0]][filter[1]]).sort((a, b) => a - b);
     minMax.push(lista[0]);
-    minMax.push(lista[lista.length] - 1);
+    minMax.push(lista[lista.length - 1]);
   }
   return minMax;
 };
